1b9799e025938a03348547f37dbf20c2cdb8be14
dokimion24 <leecy3610@gmail.com>
2023-08-12 22:46:49 +0900

    design: 폰트 사이즈 지정

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,10 +3,16 @@ import emotionReset from 'emotion-reset';
 
 export const globalStyle = css`
   ${emotionReset}
+
   * {
     margin: 0;
     padding: 0;
   }
+
+  html {
+    font-size: 10px;
+  }
+
   body {
     box-sizing: border-box;
   }
