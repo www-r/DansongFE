178c8178cd213bcc49294f29ed4160275b24a6c5
youngeun <jessicak0828@gmail.com>
2023-08-14 16:41:20 +0900

    feat: 컴포넌트 Swiper 생성

---
+++ src/components/Swiper/style.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const SlideContainer = styled.div`
+  margin: auto;
+
+  height: ${({ height }) => height + 'rem'};
+`;
+
+export const Slide = styled.div`
+  width: min-content;
+`;
