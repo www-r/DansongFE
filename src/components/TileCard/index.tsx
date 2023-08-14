21c33bc0010b1700d1731a240b67807b2932990d
youngeun <jessicak0828@gmail.com>
2023-08-14 17:38:26 +0900

    feat: 컴포넌트 TileCard 생성

---
+++ src/components/TileCard/index.tsx
@@ -0,0 +1,8 @@
+import * as S from './style';
+export default function TileCard({ width = '48.2rem', height = '76.6rem' }) {
+  return (
+    <S.TileCard width={width} height={height}>
+      TileCard
+    </S.TileCard>
+  );
+}
