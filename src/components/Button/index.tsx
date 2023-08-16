dc8532a74e043fcc2fe464a4dea8dc4df28294f5
dokimion24 <leecy3610@gmail.com>
2023-08-16 14:31:57 +0900

    feat: Button 컴포넌트 생성

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,15 @@
-export default function Button() {
-  return <div>Button</div>;
+import * as S from './style';
+
+type Props = {
+  text: string;
+  type: 'scroll' | 'link';
+  onClick: () => void;
+};
+
+export default function Button({ text, type, onClick }: Props) {
+  return (
+    <S.Button onClick={onClick} className={type}>
+      {text}
+    </S.Button>
+  );
 }
