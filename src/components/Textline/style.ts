45024eafaf7b15432e366cf0a418c0fd9957f6cb
dokimion24 <leecy3610@gmail.com>
2023-08-16 19:29:08 +0900

    feat: div -> span 변경

--- src/components/Textline/style.ts
+++ src/components/Textline/style.ts
@@ -2,13 +2,13 @@ import styled from '@emotion/styled';
 
 import { motion, HTMLMotionProps } from 'framer-motion';
 
-type TextlineProps = HTMLMotionProps<'div'> & {
+type TextlineProps = HTMLMotionProps<'span'> & {
   fontSize?: string;
   color?: string;
   isFontBold?: boolean;
 };
 
-export const Textline = styled(motion.div)<TextlineProps>`
+export const Textline = styled(motion.span)<TextlineProps>`
   display: flex;
   justify-content: center;
   align-items: center;
