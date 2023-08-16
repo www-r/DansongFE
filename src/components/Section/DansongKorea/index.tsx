a8846daaf3bca3a0d8d93284bf3f9b30298e58aa
dokimion24 <leecy3610@gmail.com>
2023-08-16 14:32:28 +0900

    feat: 비디오 태그 레이아웃 수정

--- src/components/Section/DansongKorea/index.tsx
+++ src/components/Section/DansongKorea/index.tsx
@@ -1,24 +1,28 @@
+import Button from '../../Button';
 import * as S from './style';
 
 export default function DansongKorea() {
   return (
     <S.Section>
       <S.Wrapper>
-        <div>
+        <S.VideoContainer>
           <iframe
-            width="560"
+            width="100%"
             height="315"
-            src="https://www.youtube.com/embed/VIDEO_ID_HERE"
-          ></iframe>
-        </div>
-        <div>
+            src="https://www.youtube.com/embed/H6jx_By_dKg"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          />
           <iframe
-            width="560"
+            width="100%"
             height="315"
-            src="https://www.youtube.com/embed/VIDEO_ID_HERE"
-          ></iframe>
-        </div>
+            src="https://www.youtube.com/embed/H6jx_By_dKg"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          />
+        </S.VideoContainer>
       </S.Wrapper>
+      {/* <Button text="sss" type="link" onClick={() => {}} /> */}
     </S.Section>
   );
 }
