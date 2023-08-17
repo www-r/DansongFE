e2bb3a5ee58a77e5cbd6d404e654cf17e33661b2
dokimion24 <leecy3610@gmail.com>
2023-08-17 14:54:07 +0900

    design: 반응형 스타일 추가

--- src/components/Section/DansongKorea/style.ts
+++ src/components/Section/DansongKorea/style.ts
@@ -4,28 +4,61 @@ import { theme } from '../../../styles/theme';
 export const Section = styled.section``;
 
 export const VideoSection = styled.section`
+  height: 53vh;
+  width: 100%;
+  margin-top: 7vh;
   background-image: linear-gradient(to bottom, ${theme.colors.navy1}, ${theme.colors.navy2});
-  display: flex;
-  justify-content: center;
+
+  @media (max-width: ${theme.viewport.tablet}) {
+    height: 63vh;
+  }
 `;
 
 export const VideoContainer = styled.div`
   display: flex;
   justify-content: center;
-  min-width: ${theme.width.maxWidth};
+  height: 100%;
+
+  & img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+
   @media (max-width: ${theme.viewport.tablet}) {
     display: block;
+    height: 50%;
+  }
+
+  @media (max-width: ${theme.viewport.mobile}) {
+    & img {
+      min-width: ${theme.viewport.mobile};
+    }
   }
 `;
 
 export const TextSection = styled.section`
-  height: 20rem;
+  height: 40vh;
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @media (max-width: ${theme.viewport.tablet}) {
+    height: 30vh;
+  }
 `;
 
-export const TextContainer = styled.div``;
+export const TextContainer = styled.div`
+  @media (max-width: ${theme.viewport.mobile}) {
+    min-width: ${theme.viewport.mobile};
+    & span {
+      font-size: 3.6rem;
+    }
+    & span + span {
+      font-size: 1.8rem;
+    }
+  }
+`;
 
 export const LogoSection = styled.section`
   background-image: linear-gradient(to bottom, ${theme.colors.navy1}, ${theme.colors.navy2});
