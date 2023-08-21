a7596a764aede168822dccae692e6d64894ac996
dokimion24 <leecy3610@gmail.com>
2023-08-21 16:50:41 +0900

    feat: 비디오 추가

--- src/components/VideoList/index.tsx
+++ src/components/VideoList/index.tsx
@@ -1,45 +1,31 @@
 import React, { useState } from 'react';
 import { VIDEO } from '../../data/video';
 import * as S from './style';
-
 export default function VideoList() {
   const [activeItem, setActiveItem] = useState(0);
-
   console.log(activeItem);
-
   const handleItemMouseEnter = (idx: number) => {
     idx === activeItem ? setActiveItem(0) : setActiveItem(idx);
   };
-
   return (
     <>
       {VIDEO.map((item, idx) => (
-        <li
-          key={item.id}
-          onMouseEnter={() => handleItemMouseEnter(idx + 1)}
-          onMouseLeave={() => setActiveItem(0)}
-        >
-          <S.Box
-            // animate={{ height: activeItem === idx ? 'auto' : 0 }}
-            transition={{
-              type: 'tween',
-              duration: 0.15,
-              ease: 'easeInOut',
-            }}
-          >
-            <img src={item.thumbnail} />
-          </S.Box>
-        </li>
+        <S.ImgContainer key={item.id} onMouseEnter={() => handleItemMouseEnter(idx + 1)}>
+          <img src={item.thumbnail} />
+        </S.ImgContainer>
       ))}
-      {activeItem && (
-        <iframe
-          width="560"
-          height="315"
-          src={VIDEO[activeItem - 1].src}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        ></iframe>
+      {activeItem !== 0 && (
+        <S.Video width="100%" height="100%" controls autoPlay onMouseLeave={() => setActiveItem(0)}>
+          <source src={VIDEO[activeItem - 1].video} type="video/mp4" />
+        </S.Video>
       )}
     </>
   );
 }
+// <iframe
+//   width="560"
+//   height="315"
+//   src={VIDEO[activeItem - 1].src}
+//   title="YouTube video player"
+//   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+// ></iframe>
