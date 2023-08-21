0c03ee412505765f45766e0602dedaa47bef2cbd
dokimion24 <leecy3610@gmail.com>
2023-08-21 18:07:39 +0900

    feat: 불필요한 코드 삭제

--- src/components/Section/DansongKorea/index.tsx
+++ src/components/Section/DansongKorea/index.tsx
@@ -1,50 +1,12 @@
-import { useState } from 'react';
 import Textline from '../../Textline';
 import * as S from './style';
-import useDebounce from '../../../hooks/useDebounce';
 import VideoList from '../../VideoList';
-
-// const src = 'https://www.youtube.com/watch?v=H6jx_By_dKg&t=2s';
-// const videoId = src.match(/(?<=\?v=)[^&]+/)![0];
-// const thumbnail = `https://img.youtube.com/vi/${videoId}/0.jpg`;
 
 export default function DansongKorea() {
   return (
     <S.Section>
       <S.VideoSection>
         <S.VideoContainer>
-          {/* <iframe
-            width="100%"
-            height="360"
-            src="https://www.youtube.com/embed/H6jx_By_dKg"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          />
-          <iframe
-            width="100%"
-            height="360"
-            src="https://www.youtube.com/embed/H6jx_By_dKg"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          /> */}
-          {/* {debouncedHoveredIndex !== 0 && (
-            <Hover>
-              <img
-                src={thumbnail}
-                onMouseEnter={() => setHoveredIndex(1)}
-                onMouseLeave={() => setHoveredIndex(null)}
-              />
-            </Hover>
-          )}
-          {debouncedHoveredIndex !== 1 && (
-            <Hover>
-              <img
-                src={thumbnail}
-                onMouseEnter={() => setHoveredIndex(0)}
-                onMouseLeave={() => setHoveredIndex(null)}
-              />
-            </Hover> */}
-          {/* )} */}
           <VideoList />
         </S.VideoContainer>
       </S.VideoSection>
