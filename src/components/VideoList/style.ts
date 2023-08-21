9fbee8087b335af5bd65fcc150b59577fbef2aab
dokimion24 <leecy3610@gmail.com>
2023-08-21 16:50:53 +0900

    design: 레이아웃 변경

--- src/components/VideoList/style.ts
+++ src/components/VideoList/style.ts
@@ -1,27 +1,29 @@
 import styled from '@emotion/styled';
 import { motion } from 'framer-motion';
-
-export const Box = styled(motion.div)`
-  height: 100%;
-
+export const Box = styled(motion.div)``;
+export const Video = styled.video`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+export const ImgContainer = styled.li`
+  display: flex;
+  align-items: center;
+  width: 100%;
   & img {
     object-fit: cover;
     width: 100%;
-    height: 100%;
   }
 `;
-
 /* & img {
     object-fit: cover;
     width: 100%;
     height: 100%;
   }
-
   @media (max-width: ${theme.viewport.tablet}) {
     display: block;
     height: 50%;
   }
-
   @media (max-width: ${theme.viewport.mobile}) {
     & img {
       min-width: ${theme.viewport.mobile};
