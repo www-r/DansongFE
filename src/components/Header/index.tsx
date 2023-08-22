18e76a615d4370592b0273d67859ec528261c5a5
dokimion24 <leecy3610@gmail.com>
2023-08-22 20:43:03 +0900

    feat: container 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,19 @@
 import DivisionLine from '../DivisionLine';
+import Logo from '../Logo';
 import * as S from './style';
 
 export default function Header() {
   return (
     <S.Header>
-      <S.Wrapper>
-        <S.Menu>DANSONG KOREA</S.Menu>
-        <S.Menu>CONTENTS</S.Menu>
-        <S.Menu>COMMERCE</S.Menu>
-        <S.Menu>CONTACT</S.Menu>
-      </S.Wrapper>
+      <S.Container>
+        <Logo logo="dansongW.png" size="small" />
+        <S.Wrapper>
+          <S.Menu>DANSONG KOREA</S.Menu>
+          <S.Menu>CONTENTS</S.Menu>
+          <S.Menu>COMMERCE</S.Menu>
+          <S.Menu>CONTACT</S.Menu>
+        </S.Wrapper>
+      </S.Container>
       <DivisionLine />
     </S.Header>
   );
