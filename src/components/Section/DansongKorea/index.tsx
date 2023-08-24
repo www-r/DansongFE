00649449b146514012ddd2bb397f1cbcc44911db
dokimion24 <leecy3610@gmail.com>
2023-08-24 18:57:19 +0900

    feat: Studio 컴포넌트 추가

--- src/components/Section/DansongKorea/index.tsx
+++ src/components/Section/DansongKorea/index.tsx
@@ -1,6 +1,7 @@
 import Textline from '../../Textline';
 import * as S from './style';
 import VideoList from '../../VideoList';
+import Studio from '../../Studio';
 
 export default function DansongKorea() {
   return (
@@ -16,21 +17,14 @@ export default function DansongKorea() {
           <Textline fontSize="4.8rem" animation="top" isFontBold={true}>
             Think Make Change
           </Textline>
-          <Textline fontSize="2.4rem" animation="top">
+          <Textline fontSize="2.4rem" animation="top" isFontBold={true}>
             생각이 실현되는 곳
           </Textline>
         </S.TextContainer>
       </S.TextSection>
 
       <S.LogoSection>
-        <S.TextContainer>
-          <Textline fontSize="28rem" animation="top" isFontBold={true} color="#fff">
-            DSS
-          </Textline>
-          <Textline fontSize="8rem" animation="top" isFontBold={true} color="#fff">
-            DANSONG STUDIOS
-          </Textline>
-        </S.TextContainer>
+        <Studio />
       </S.LogoSection>
     </S.Section>
   );
