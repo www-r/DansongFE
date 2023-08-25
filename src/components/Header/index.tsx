610d1f7e519f1351b56072b0befa853f67504030
dokimion24 <leecy3610@gmail.com>
2023-08-25 20:01:01 +0900

    feat: Wrapper 이름 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,12 +7,12 @@ export default function Header() {
     <S.Header>
       <S.Container>
         <Logo logo="dansongW.png" size="small" />
-        <S.Wrapper>
+        <S.MenuContainer>
           <S.Menu>DANSONG KOREA</S.Menu>
           <S.Menu>CONTENTS</S.Menu>
           <S.Menu>COMMERCE</S.Menu>
           <S.Menu>CONTACT</S.Menu>
-        </S.Wrapper>
+        </S.MenuContainer>
       </S.Container>
       <DivisionLine />
     </S.Header>
