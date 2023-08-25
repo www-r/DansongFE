a4147b4e1cd3437e3f443726cee4a4791c9ac7c9
dokimion24 <leecy3610@gmail.com>
2023-08-25 21:34:29 +0900

    feat: isBoxShadow props 추가

--- src/components/Circle/index.tsx
+++ src/components/Circle/index.tsx
@@ -6,13 +6,14 @@ import { HTMLMotionProps } from 'framer-motion';
 type Props = {
   size: 'small' | 'medium' | 'large';
   bgColor?: string;
+  isBoxShadow?: boolean;
   children: React.ReactNode;
   style?: CSSProperties;
 } & HTMLMotionProps<'div'>;
 
-export default function Circle({ size, bgColor, children, style }: Props) {
+export default function Circle({ size, bgColor, children, isBoxShadow = false, style }: Props) {
   return (
-    <S.Container className={size} bgColor={bgColor} style={style}>
+    <S.Container className={size} bgColor={bgColor} isBoxShadow={isBoxShadow} style={style}>
       {children}
     </S.Container>
   );
