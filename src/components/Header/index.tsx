98b1575065c8ad6a6c444d5aa43a839f40eaaa3a
youngeun <jessicak0828@gmail.com>
2023-08-29 23:59:50 +0900

    fix: Header Logo 이미지 경로 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,11 @@ export default function Header() {
   return (
     <S.Header>
       <S.Container>
-        <Logo logo="dansongW.png" size="small" />
+        <Logo
+          logo="dansong"
+          size="small"
+          src="https://dansong-s3.s3.ap-northeast-2.amazonaws.com/logos/dansongW.png"
+        />
         <S.MenuContainer>
           <S.Menu>DANSONG KOREA</S.Menu>
           <S.Menu>CONTENTS</S.Menu>
