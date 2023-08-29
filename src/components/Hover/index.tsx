0a33bdee6c79205acc553a65957246fcaf74df57
youngeun <jessicak0828@gmail.com>
2023-08-29 13:58:10 +0900

    fix: 사용하지 않는 코드들 삭제( eslint error)

--- src/components/Hover/index.tsx
+++ src/components/Hover/index.tsx
@@ -1,17 +1,16 @@
-import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 
 type Props = {
   children: React.ReactNode;
 };
 
-const variants = {
-  open: { opacity: 1, x: 0 },
-  closed: { opacity: 0, x: '-100%' },
-};
+// const variants = {
+//   open: { opacity: 1, x: 0 },
+//   closed: { opacity: 0, x: '-100%' },
+// };
 
 export default function Hover({ children }: Props) {
-  const [isHovered, setHovered] = useState(false);
+  // const [isHovered, setHovered] = useState(false);
 
   return (
     <motion.div
