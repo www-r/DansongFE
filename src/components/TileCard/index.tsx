0730b44212e58d1d72dfd0962e468b376b9cdd71
youngeun <jessicak0828@gmail.com>
2023-08-29 13:53:58 +0900

    delete

--- src/components/TileCard/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as S from './style';
-export default function TileCard({ width = '40vw', height = '70rem' }) {
-  return (
-    <S.TileCard width={width} height={height}>
-      TileCard
-    </S.TileCard>
-  );
-}
