fbef913dcc06deb2f0909f16ed9e1569946ae287
dokimion24 <leecy3610@gmail.com>
2023-09-04 18:28:12 +0900

    feat: 영상 s3 가져 오기

--- src/data/video.ts
+++ src/data/video.ts
@@ -1,16 +1,14 @@
-import video1 from '/videos/video1.mp4';
-import video2 from '/videos/video2.mp4';
 import thumbnail1 from '/images/thumbnail1.jpg';
 import thumbnail2 from '/images/thumbnail2.jpg';
 export const VIDEO: Video[] = [
   {
     id: 0,
-    video: video1,
+    video: 'https://dansong-s3.s3.ap-northeast-2.amazonaws.com/videos/video1.mp4',
     thumbnail: thumbnail1,
   },
   {
     id: 1,
-    video: video2,
+    video: 'https://dansong-s3.s3.ap-northeast-2.amazonaws.com/videos/video2.mp4',
     thumbnail: thumbnail2,
   },
 ];
