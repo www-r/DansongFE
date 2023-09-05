57e5a23f94862604a0d97f56defc7dd718882cc0
dokimion24 <leecy3610@gmail.com>
2023-09-05 22:01:11 +0900

    feat: type 추가

--- src/components/FlipCard/Front/index.tsx
+++ src/components/FlipCard/Front/index.tsx
@@ -1,4 +1,4 @@
 import * as S from './style';
-export default function FlipCardFront({ children }) {
+export default function FlipCardFront({ children }: { children: React.ReactNode }) {
   return <S.Front>{children}</S.Front>;
 }
