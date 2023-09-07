eb5e7c125bc024e2ace7b783b7e461efbf92dcf6
dokimion24 <leecy3610@gmail.com>
2023-09-07 13:47:12 +0900

    feat: width, color props 추가

--- src/components/DivisionLine/index.tsx
+++ src/components/DivisionLine/index.tsx
@@ -1,9 +1,16 @@
+import { theme } from '../../styles/theme';
 import * as S from './style';
 
 type Props = {
   borderStyle?: string;
+  width?: string;
+  color?: string;
 };
 
-export default function DivisionLine({ borderStyle = 'solid' }: Props) {
-  return <S.DivisionLine borderStyle={borderStyle} />;
+export default function DivisionLine({
+  borderStyle = 'solid',
+  width = '100%',
+  color = theme.colors.border,
+}: Props) {
+  return <S.DivisionLine borderStyle={borderStyle} width={width} color={color} />;
 }
