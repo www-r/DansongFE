7e37a51dafbc046bb974113f56559354bbacb884
youngeun <jessicak0828@gmail.com>
2023-09-13 02:35:24 +0900

    feat: Footer 생성

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,27 @@
-
+import { useState } from 'react';
 import * as S from './style';
 
 export default function Footer() {
   return (
     <S.Footer>
-      Footer
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+      <img className="footer--logo" src="/logos/dansongB.png" alt="logo" height={100} />
+      <div className="footer--text">
+        <div className="text--top">
+          <div>
+            <p className="company-name">Dansong Inc.(단송)</p>
+            <p className='flexwrap'>
+              <span>대표자: 김동영 | </span>
+              <span>전화번호: 010-9928-2881</span>
+            </p>
+            <p>
+              <span>사업자 등록번호: 622-03-75466 | </span>
+              <span>통신판매업신고번호: 2019-서울강남-00565</span>
+            </p>
+          </div>
+          <div className="sns-container"></div>
+        </div>
+        ​​<p className="text--bottom">© 2019 Dansong. All rights reserved.</p>
+      </div>
     </S.Footer>
   );
 }
--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,5 +1,29 @@
 import styled from '@emotion/styled';
 
 export const Footer = styled.footer`
+  display: flex;
+  gap: 5rem;
   background-color: ${({ theme }) => theme.backgroundColor.footer};
+  color: ${({ theme }) => theme.textColor.footer};
+  font-size: 1.3rem;
+  padding: 2rem 10vw;
+  line-height: 1.6;
+  flex-wrap: wrap;
+  .footer--logo {
+    @media (max-width: ${({ theme }) => theme.viewport.desktop}) {
+      display: none;
+    }
+  }
+  .text--top {
+    display: grid;
+    grid-template-columns: 4fr 1fr;
+    gap: 5rem;
+  }
+  .text--bottom {
+    color: ${({ theme }) => theme.textColor.grey};
+  }
+  .flexwrap {
+    display: flex;
+    flex-wrap: wrap;
+  }
 `;
