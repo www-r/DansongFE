cb931b45880a1321b9413cb0868e31e931207582
dokimion24 <leecy3610@gmail.com>
2023-09-15 11:41:51 +0900

    design: 스타일 변경

--- src/pages/ContactPage/style.ts
+++ src/pages/ContactPage/style.ts
@@ -2,4 +2,5 @@ import styled from '@emotion/styled';
 
 export const Main = styled.main`
   width: 100%;
+  background-color: ${({ theme }) => theme.backgroundColor.white};
 `;
--- src/pages/MainPage/style.ts
+++ src/pages/MainPage/style.ts
@@ -2,5 +2,4 @@ import styled from '@emotion/styled';
 
 export const Main = styled.main`
   width: 100%;
-  padding-top: 6rem;
 `;
