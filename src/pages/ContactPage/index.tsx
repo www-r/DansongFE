8872354e81d53eba551250c85ff2d6446d707241
dokimion24 <leecy3610@gmail.com>
2023-09-15 11:41:28 +0900

    feat: ContactInfo 컴포넌트 생성

--- src/pages/ContactPage/index.tsx
+++ src/pages/ContactPage/index.tsx
@@ -1,35 +1,14 @@
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import * as S from './style';
-import Map from '../../components/Map';
+import ContactInfo from '../../components/ContactInfo';
 
 export default function ContactPage() {
   return (
     <>
       <Header />
       <S.Main>
-        <h3>CONTACT INFO</h3>
-        <ul>
-          <li>
-            <label htmlFor="contact--mobile">전화</label>
-            <a href="tel:070-7799-0703" id="contact--mobile">
-              070 7799 0703
-            </a>
-          </li>
-          <li>
-            <label htmlFor="contact--email">이메일</label>
-            <a href="mailto:official@dansong.kr" id="contact--email">
-              official@dansong.kr
-            </a>
-          </li>
-          <li>
-            <label htmlFor="contact--address">주소</label>
-            <a href="javascript:void(0)" id="contact--address">
-              212, Hakdong-ro, Gangnam-gu, Postville 703
-            </a>
-          </li>
-        </ul>
-        <Map />
+        <ContactInfo />
       </S.Main>
       <Footer />
     </>
