7f979070e61b53e9a5dc323bfee04bb0a8c71947
youngeun <jessicak0828@gmail.com>
2023-09-16 18:49:08 +0900

    fix: Contact Icon a태그에서 Link태그로 변경

--- src/components/ContactIcon/index.tsx
+++ src/components/ContactIcon/index.tsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import * as S from './style';
 
 export default function ContactIcon() {
   return (
-    <a href="/contact">
+    <Link to="/contact">
       <S.ContactIcon>
         <img src="/images/contactIcon.svg" alt="contact Icon" width={50} />
       </S.ContactIcon>
-    </a>
+    </Link>
   );
 }
