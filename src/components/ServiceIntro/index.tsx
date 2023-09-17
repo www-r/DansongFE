a61731014cb395959fd075fcefa584c875558df8
youngeun <jessicak0828@gmail.com>
2023-09-17 21:08:14 +0900

    feat: 로고 클릭 시 해당 구간으로 이동

--- src/components/ServiceIntro/index.tsx
+++ src/components/ServiceIntro/index.tsx
@@ -27,29 +27,33 @@ export default function ServiceIntro() {
             <a href="/#contents">
               <Logo logo="dansong" src="/images/DSS.png" size="ml" />
             </a>
-            <S.MiddleContainer direction="column">
-              <Textline fontSize={2.4} animation={isMobile ? 'none' : 'top'} isFontBold={true}>
-                컨텐츠
-              </Textline>
-              <DivisionLine borderStyle="dashed" />
-              <Textline fontSize={1.8} animation={isMobile ? 'none' : 'bottom'} isFontBold={true}>
-                DSS STUDIO
-              </Textline>
-            </S.MiddleContainer>
+            <a href="/#contents">
+              <S.MiddleContainer direction="column">
+                <Textline fontSize={2.4} animation={isMobile ? 'none' : 'top'} isFontBold={true}>
+                  컨텐츠
+                </Textline>
+                <DivisionLine borderStyle="dashed" />
+                <Textline fontSize={1.8} animation={isMobile ? 'none' : 'bottom'} isFontBold={true}>
+                  DSS STUDIO
+                </Textline>
+              </S.MiddleContainer>
+            </a>
           </CenterContainer>
           <CenterContainer direction="column" style={{ gap: '1rem' }}>
             <a href="/#contents">
               <Logo logo="dansong" src="/logos/itreaming1.png" size="ml" />
             </a>
-            <S.MiddleContainer direction="column">
-              <Textline fontSize={2.4} animation={isMobile ? 'none' : 'top'} isFontBold={true}>
-                커머스
-              </Textline>
-              <DivisionLine borderStyle="dashed" />
-              <Textline fontSize={1.8} animation={isMobile ? 'none' : 'bottom'} isFontBold={true}>
-                잇트리밍
-              </Textline>
-            </S.MiddleContainer>
+            <a href="/#commerce">
+              <S.MiddleContainer direction="column">
+                <Textline fontSize={2.4} animation={isMobile ? 'none' : 'top'} isFontBold={true}>
+                  커머스
+                </Textline>
+                <DivisionLine borderStyle="dashed" />
+                <Textline fontSize={1.8} animation={isMobile ? 'none' : 'bottom'} isFontBold={true}>
+                  잇트리밍
+                </Textline>
+              </S.MiddleContainer>
+            </a>
           </CenterContainer>
         </CenterContainer>
       </S.Container>
